90144b57775af77ad4dd95d039acac40843715a5
Andrey Lepikhov (auto) <andrey.lepikhov@devexpress.com>
2023-07-11 17:13:23 +0400

    auto sync for version 2023.1

--- CS/NorthwindModel.cs
+++ CS/NorthwindModel.cs
@@ -1,14 +1,20 @@
-﻿using System;
+using NorthwindModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Runtime.Remoting.Contexts;
 using System.Text;
-using T264297.NwindServiceReference;
 
 namespace T264297 {
-    class NorthwindModel {
-        NorthwindEntities context = new NorthwindEntities(new Uri("http://services.odata.org/V3/Northwind/Northwind.svc"));
-        public List<Category> Categories() {
-            return context.Categories.Expand("Products").ToList();
+    public class NorthwindModel {
+        public IEnumerable<Category> Categories()
+        {
+            ServicePointManager.Expect100Continue = true;
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+            NorthwindEntities context = new NorthwindEntities(new Uri("https://services.odata.org/V3/Northwind/Northwind.svc"));
+            IEnumerable<Category> categories = context.Categories.Execute();
+            return categories;
         }
     }
 }
